a79143947814d75e0377ff9cb14b3e2641a27b94
cglaeser <glaeser@7says.de>
2014-03-22 19:38:22 +0100

    testProject added to Maven

---
+++ testProject/src/main/java/org/foodscanner/testProject/App.java
@@ -0,0 +1,13 @@
+package org.foodscanner.testProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
